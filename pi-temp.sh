9686fca0c84cbe1ed11257ed3109bf807edb6985
Adrian <23638074+sysadmin-info@users.noreply.github.com>
2022-02-04 17:03:26 +0100

    Update pi-temp.sh

--- pi-temp.sh
+++ pi-temp.sh
@@ -4,12 +4,14 @@
 # Author: Adrian Ambroziak <sysadmin.info.pl> under GPL v3.x+
 # -------------------------------------------------------
 homedir="$(getent passwd $( /usr/bin/id -u ) | cut -d: -f6)"
+OutputFile="temperature.log"
+concatenate="${homedir}/${OutputFile}"
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 while true; do
 echo "$(date) @ $(hostname)" >> /$homedir/temp.log
-echo "-------------------------------------------" >> /$homedir/temp.log
-echo "GPU => $(/usr/bin/vcgencmd measure_temp)" | sed 's/temp=//' >> /$homedir/temp.log
-echo "CPU => $((cpu/1000))'C" >> /$homedir/temp.log
-echo "-------------------------------------------" >> /$homedir/temp.log
+echo "-------------------------------------------" >> $concatenate
+echo "GPU => $(/usr/bin/vcgencmd measure_temp)" | sed 's/temp=//' >> concatenate
+echo "CPU => $((cpu/1000))'C" >> concatenate
+echo "-------------------------------------------" >> concatenate
 sleep 2;
 done
